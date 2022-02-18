ec518da2798be13f3124729c6dbf4891c56292e5
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-18 11:30:24 +0300

    create route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});New 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'home'});
 });
-
-module.exports = router;
+router.get("/", function (req, res, next) {
+res.render("index", { page: "Home", menuId: "home" });
+});
+router.get("/about", function (req, res, next) {
+res.render("about", { page: "About", menuId: "about" });
+});
+router.get("/contact", function (req, res, next) {
+res.render("contact", { page: "Contact", menuId: "contact" });
+});
