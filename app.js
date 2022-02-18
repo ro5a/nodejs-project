c1f7fb2dea6ad885f6a0e85774e727f3ae39de26
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-18 20:40:30 +0300

    route detailes

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/category", function(req, res){
 
   res.render("category"); 
 });
+app.get("/detailes", function(req, res){
+
+  res.render("detailes"); 
+});
 app.listen(port, function(){
      console.log("server is running on port" + port);
 });
